196f5975939027932d4bfba08e6cf871d6fabfaf
Jose Rico <josearudeveloper@gmail.com>
2025-01-22 17:08:02 +0100

    fixed: keyprop

--- src/pages/Home/UpcomingActivities.tsx
+++ src/pages/Home/UpcomingActivities.tsx
@@ -3,7 +3,7 @@ import Activity from "@/components/Activity"
 const UpcomingActivities = () => {
 
   const upcomingActivities = [
-    {
+    { id:1,
       title: 'Palabra Vision',
       description: '"Ensancha" es un llamado a la expansión de la iglesia, basado en Isaías 54:2. Nos invita a extendernos espiritualmente y territorialmente, firmes en nuestra fe y obediencia a Dios, confiando en Su respaldo para llevar el evangelio a más vidas y comunidades.',
       place: "Plaza de Castilla",
@@ -11,6 +11,7 @@ const UpcomingActivities = () => {
       image: "/palabraVision2025.jpeg",
     },
     {
+      id:2,
       title: "Aniversario de 4C",
       description: "¡11 años de fidelidad de Dios! Acompáñanos este 8 de febrero con el Pastor Juan Carlos Escobar, presidente de las ADE. ¡Te esperamos!",
       place: "Auditorio 4C Parla, Calle Londres 58, Parla",
@@ -18,6 +19,7 @@ const UpcomingActivities = () => {
       image: "/undecimoAniversario.jpeg",
     },
     {
+      id:3,
       title: "Retiro Familiar",
       description: "Del 17 al 19 de abril, vive un tiempo de conexión espiritual y familiar. Fortalezcamos nuestra fe, renovemos nuestro compromiso con Dios y disfrutemos juntos momentos especiales.",
       place: "Plaza de Castilla",
@@ -34,7 +36,7 @@ const UpcomingActivities = () => {
         <div className="flex flex-col gap-8">
           {
             upcomingActivities.map((activity) => (
-              <Activity button={true} description={activity.description}
+              <Activity key={activity.id} button={true} description={activity.description}
                 place={activity.place} hour={activity.hour} image={activity.image} title={activity.title} />
             ))
           }
