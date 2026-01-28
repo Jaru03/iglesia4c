927a23824b82b399cb6a7a2bf1ef8b4df543d492
arthur_san <arthur.x7891@gmail.com>
2026-01-28 17:03:02 +0100

    Agregado modulo de Actividades y Seguridad Final

--- src/components/admin/Sidebar.tsx
+++ src/components/admin/Sidebar.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
       {/* LOGO */}
       <div className="p-6 border-b border-slate-800">
         <h2 className="text-2xl font-bold flex items-center gap-2">
-          Iglesia Casa De Dios 
+          Casa de Dios
         </h2>
         <span className="text-xs text-slate-400 uppercase tracking-wider">Panel Admin</span>
       </div>
@@ -37,6 +37,12 @@ export default function Sidebar() {
         <Link href="/admin/predicas" className="flex items-center gap-3 px-4 py-3 text-slate-300 hover:bg-slate-800 hover:text-white rounded-xl transition-all">
           <span></span> Prédicas
         </Link>
+
+        <Link  href="/admin/actividades" className="flex items-center gap-3 p-3 text-slate-300 hover:bg-slate-800 hover:text-white rounded-lg transition-all"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+        <span>Calendario</span>
+      </Link>
       </nav>
 
       {/* FOOTER DEL SIDEBAR */}
