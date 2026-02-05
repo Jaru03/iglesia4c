d1b87a412b2a16ae61909882194e7de9d2dad539
Jose Rico <josearudeveloper@gmail.com>
2026-02-05 11:01:38 +0100

    intent to redeploy

--- src/app/api/activities/route.ts
+++ src/app/api/activities/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "@/utils/prisma";
 export async function GET() {
   try {
     const activities = await prisma.activities.findMany({
