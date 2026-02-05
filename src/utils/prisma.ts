035d25e3421feefe5f075f9eb50c7708f09d6ac9
Jose Rico <josearudeveloper@gmail.com>
2026-02-05 10:36:51 +0100

    intent to redeploy

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,18 +1,24 @@
 import { PrismaClient } from '../../generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL no está definida')
 }
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
+}
+
 const prisma = globalForPrisma.prisma || new PrismaClient({
   adapter,
 })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
