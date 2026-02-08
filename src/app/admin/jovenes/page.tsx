4df629a68ab44bc9ce4bfc3a961254c51782655b
arthur_san <arthur.x7891@gmail.com>
2026-02-08 21:34:28 +0100

    fix:jovenes

--- src/app/admin/jovenes/page.tsx
+++ src/app/admin/jovenes/page.tsx
@@ -1,15 +1,15 @@
 import prisma from "@/utils/prisma";
 import ListadoJovenesCliente from "./ListadoJovenesCliente";
 
-// 1. Esta es una Server Component (sin "use client")
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function JovenesPage() {
-  // 2. Traemos los datos reales de Supabase
   const jovenesReales = await prisma.joven.findMany({
     orderBy: {
-      nombres: 'asc'
-    }
+      nombres: "asc",
+    },
   });
 
-  // 3. Se los pasamos al componente que tiene el buscador
   return <ListadoJovenesCliente jovenesIniciales={jovenesReales} />;
 }
