abf95bc6d694ac78e4c6be60a5e13fd5504c96ac
arthur_san <arthur.x7891@gmail.com>
2026-02-12 17:47:09 +0100

    Hacer publica la home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,16 @@
-// middleware.ts
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
-    // 1. Sacamos el rol del token
     const role = req.nextauth.token?.role as string;
     const path = req.nextUrl.pathname;
-    
-    // --- REGLAS PARA EL LIDER ---
-    // Si es LIDER y quiere entrar al Admin o a la Home -> ¡Al Kiosco!
-    if ((path.startsWith("/admin") || path === "/") && role === "LIDER") {
+
+    if (path.startsWith("/admin") && role === "LIDER") {
       return NextResponse.redirect(new URL("/kiosko", req.url));
     }
 
-    // --- REGLAS PARA EL ADMIN (NORMAL) ---
-    // Si es ADMIN y quiere entrar a "Equipo" o "Usuarios" -> ¡Bloqueado!
-    // (Asumo que tu ruta para crear admins es /admin/equipo o /admin/usuarios)
     if (path.startsWith("/admin/equipo") && role === "ADMIN") {
-       // Lo mandamos de vuelta al dashboard principal, no tiene permiso aquí
        return NextResponse.redirect(new URL("/admin", req.url));
     }
 
@@ -33,8 +25,7 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/",            
-    "/admin/:path*", 
-    "/kiosko/:path*", 
+    "/admin/:path*",
+    "/kiosko/:path*",
   ],
 };
